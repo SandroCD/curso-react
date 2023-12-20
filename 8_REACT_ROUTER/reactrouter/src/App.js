d8d8ee588584c2cce9034177b57fd85e4f593990
SandroCD <sandrocd@gmail.com>
2023-12-20 15:54:01 -0300

    Configurando React Router

--- 8_REACT_ROUTER/reactrouter/src/App.js
+++ 8_REACT_ROUTER/reactrouter/src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+
+// 1 - CONFIGURANDO ROUTER
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// PAGES
+import Home from "./pages/Home";
+import About from './pages/About';
 
 function App() {
   return (
     <div className="App">
       <h1>React Router</h1>
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
