823ac793f0c6372278e48e900b1e306c19afe164
SandroCD <sandrocd@gmail.com>
2023-12-21 17:01:32 -0300

    Criação projeto Context

---
+++ 9_CONTEXT/context/src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+// PAGES
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Context</h1>
+      <BrowserRouter >
+        <Navbar />
+        <Routes >
+          <Route path="/" element={<Home />}/>
+          <Route path="/about/" element={<About />} />
+          <Route path="products" element={<Products />}/>
+        </Routes>
+      </BrowserRouter>      
+    </div>
+  );
+}
+
+export default App;
