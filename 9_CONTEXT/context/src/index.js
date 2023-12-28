2308c399af1d15732d82f404319ce3142870f39f
SandroCD <sandrocd@gmail.com>
2023-12-28 09:13:58 -0300

    Inserindo Counter Context

--- 9_CONTEXT/context/src/index.js
+++ 9_CONTEXT/context/src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { CounterContextProvider } from './context/CounterContext';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* 2 - CRIANDO PROVIDER */}
+    <CounterContextProvider>
+      <App />
+    </CounterContextProvider>
   </React.StrictMode>
 );
 
