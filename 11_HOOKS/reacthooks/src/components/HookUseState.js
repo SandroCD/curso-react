5c6da820cb4c51d363786421f6bac170e4858f02
SandroCD <sandrocd@gmail.com>
2024-01-25 10:55:07 -0300

    UseState e inputs

--- 11_HOOKS/reacthooks/src/components/HookUseState.js
+++ 11_HOOKS/reacthooks/src/components/HookUseState.js
@@ -8,12 +8,25 @@ const HookUseState = () => {
 
     const changeNames = () => {
 
-        userName = "Kauã Luiz"
+        userName = "Kauã Luiz";
 
-        setName("Sandro Câmara Damasceno")
-    }
-  
-  
+        setName("Sandro Câmara Damasceno");
+
+        console.log(userName);
+    };
+
+    console.log(name)
+
+    //  2 - useState e input
+    const [age, setAge] = useState(18)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        // envio a uma API
+        console.log(age);
+    };
+    
     return (
         <div>
             {/* 1 - useState */}
@@ -21,6 +34,17 @@ const HookUseState = () => {
             <p>Variável: {userName}</p>
             <p>useState: {name}</p>
             <button onClick={changeNames}>Mudar nomes!</button>
+            {/* 2 - useState e input */}
+            <p>Digite sua idade:</p>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={age}
+                    onChange={(e) => setAge(e.target.value)}
+                />
+                <input type="submit" value="Enviar" />
+            </form>
+            <p>Você tem {age} anos!</p>
             <hr />
         </div>
     );
