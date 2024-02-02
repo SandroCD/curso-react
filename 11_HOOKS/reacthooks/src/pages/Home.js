1452c7ff07ca762dacc1ca74c54ec780f53ac26c
SandroCD <sandrocd@gmail.com>
2024-02-02 15:33:27 -0300

    UseContext

--- 11_HOOKS/reacthooks/src/pages/Home.js
+++ 11_HOOKS/reacthooks/src/pages/Home.js
@@ -3,14 +3,23 @@ import HookUseState from '../components/HookUseState';
 import HookUseReducer from '../components/HookUseReducer';
 import HookUseEffect from '../components/HookUseEffect';
 
+// useContext
+import { useContext } from 'react';
+import { SomeContext } from '../components/HookUseContext';
+
 const Home = () => {
-  return (
+  const { contextValue } = useContext(SomeContext);
+
+    return (
     <div>
       <HookUseState />
       <HookUseReducer />
       <HookUseEffect />
+      <h2>useContext</h2>
+      <p>Valor do Contexto: {contextValue}</p>
+      <hr />
     </div>
-  )
-}
+  );
+};
 
 export default Home
